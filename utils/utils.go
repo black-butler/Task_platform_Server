e1fcc3823ed68fe0bc707784da6e35b7d8fdd1ac
butler <3204235868@qq.com>
2022-03-13 18:27:27 +0800

    判断某个float64是不是整数

--- utils/utils.go
+++ utils/utils.go
@@ -3,3 +3,12 @@ package utils
 const (
 	Time_Format = "2006-01-02 15:04:05"
 )
+
+//判断某个float64是不是整数
+func Check_float64_zheng(zhi float64) bool {
+	if zhi == float64(int(zhi)) {
+		return true
+	} else {
+		return false
+	}
+}
